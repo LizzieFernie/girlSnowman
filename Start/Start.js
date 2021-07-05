c229b721016c477332955bd3859afedb560525a2
fernandeslizanne <53610361+LizzieFernie@users.noreply.github.com>
2021-07-05 15:12:31 +0530

    Add files via upload

---
+++ Start/Start.js
@@ -0,0 +1,38 @@
+/* eslint-disable require-yield, eqeqeq */
+
+import {
+  Sprite,
+  Trigger,
+  Watcher,
+  Costume,
+  Color,
+  Sound
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+export default class Start extends Sprite {
+  constructor(...args) {
+    super(...args);
+
+    this.costumes = [
+      new Costume("start", "./Start/costumes/start.svg", { x: 160, y: 55 })
+    ];
+
+    this.sounds = [];
+
+    this.triggers = [
+      new Trigger(Trigger.CLICKED, this.whenthisspriteclicked),
+      new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked)
+    ];
+  }
+
+  *whenthisspriteclicked() {
+    this.broadcast("startGame");
+    this.visible = false;
+    this.stage.costume = "board";
+    this.broadcast("board");
+  }
+
+  *whenGreenFlagClicked() {
+    this.visible = true;
+  }
+}
